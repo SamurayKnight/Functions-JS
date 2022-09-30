729ea0bfe0521599be03772cb21373c8a70acb92
SamurayKnight <danlee97.06@gmail.com>
2022-09-30 13:00:59 -0500

    Función_3

--- functions.js
+++ functions.js
@@ -25,10 +25,10 @@ cutString("Hello World",5);
 //Función que devuelve un array con separación.
 const toArray = (string = "", separator = undefined) => (!string) 
 ? console.warn(`El valor ${string} no es valido.`)
-: (separator === undefined+) 
+: (separator === undefined) 
 ? console.warn(`El valor ${separator} no es valido.`)
 : console.info(string.split(separator));
 
 
 toArray();
-toArray("Hola mundo Hola Mundo","-");
+toArray("Hola mundo Hola Mundo"," ");
