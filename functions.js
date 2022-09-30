ff5cd7f760dcb2dfca7538f2944ed94fc863d402
SamurayKnight <danlee97.06@gmail.com>
2022-09-30 14:02:47 -0500

    Función_4

--- functions.js
+++ functions.js
@@ -12,7 +12,7 @@ countString("STRING");
 
 //Función que devuelve un string dado recortado n veces.
 const cutString = (string ="", number = NaN) => (!string)
-? console.warn(`El valor ${string} no es valido.`)
+? console.warn(`El valor${string} no es valido.`)
 : console.info(string.slice(0, number));
 
 
@@ -24,7 +24,7 @@ cutString("Hello World",5);
 
 //Función que devuelve un array con separación.
 const toArray = (string = "", separator = undefined) => (!string) 
-? console.warn(`El valor ${string} no es valido.`)
+? console.warn(`El valor${string} no es valido.`)
 : (separator === undefined) 
 ? console.warn(`El valor ${separator} no es valido.`)
 : console.info(string.split(separator));
@@ -32,3 +32,33 @@ const toArray = (string = "", separator = undefined) => (!string)
 
 toArray();
 toArray("Hola mundo Hola Mundo"," ");
+
+
+
+
+//Función que repite un texto n veces.
+const repeat = (text = "", number = undefined) => {
+    if (!text) {
+        console.warn(`Valor${text} invalido, debe añadir un texto.`);
+    }
+    if (number === undefined) {
+        console.warn(`Valor invalido, debe añadir un número.`);
+    };
+
+    //bucle
+    let i = 1;
+    while (i <= number) {
+        console.info(text);
+        i++;
+    }
+    
+};
+
+
+repeat();
+repeat("hola", 2);
+
+
+
+
+//
