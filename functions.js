67ce4f42e9686c63d58b10a589d8a72486922332
SamurayKnight <danlee97.06@gmail.com>
2022-09-30 13:00:14 -0500

    Función_3

--- functions.js
+++ functions.js
@@ -3,8 +3,10 @@ const countString = (string = "") => (!string)
 ? console.warn(`El dato${string} no es valido.`) 
 : console.info(`Tu string ${string} tiene ${string.length} caractéres.`);
 
+
 countString();
 countString("STRING");
+
 
 
 
@@ -13,8 +15,20 @@ const cutString = (string ="", number = NaN) => (!string)
 ? console.warn(`El valor ${string} no es valido.`)
 : console.info(string.slice(0, number));
 
+
 cutString();
 cutString("Hello World",5);
 
 
 
+
+//Función que devuelve un array con separación.
+const toArray = (string = "", separator = undefined) => (!string) 
+? console.warn(`El valor ${string} no es valido.`)
+: (separator === undefined+) 
+? console.warn(`El valor ${separator} no es valido.`)
+: console.info(string.split(separator));
+
+
+toArray();
+toArray("Hola mundo Hola Mundo","-");
