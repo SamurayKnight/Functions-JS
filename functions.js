0c4cc7918010333431ffade9efe6a130ed371474
SamurayKnight <danlee97.06@gmail.com>
2022-09-30 11:11:40 -0500

    Función_2

--- functions.js
+++ functions.js
@@ -1,7 +1,20 @@
 //Función que cuenta el número de caracteres de un string.
 const countString = (string = "") => (!string) 
-? console.warn(`El dato ${string} no es valido.`) 
-: console.info(`Tu string tiene ${string.length}.`);
+? console.warn(`El dato${string} no es valido.`) 
+: console.info(`Tu string ${string} tiene ${string.length} caractéres.`);
 
 countString();
-countString("STRING")
+countString("STRING");
+
+
+
+//Función que devuelve un string dado recortado n veces.
+const cutString = (string ="", number = NaN) => (!string)
+? console.warn(`El valor ${string} no es valido.`)
+: console.info(string.slice(0, number));
+
+cutString();
+cutString("Hello World",5);
+
+
+
