7f2c3c9cc66070a786000e17ebdf6aeca2a1bb67
SamurayKnight <danlee97.06@gmail.com>
2022-11-30 09:17:47 -0600

    Menu-hamburguesa

--- DOM-functions.js
+++
@@ -1,6 +0,0 @@
-//Crear un menú tipo "hamburguesa"
-const $nav = document.querySelector(".navegador"),
-      $section = document.querySelector(".section"),
-      $button = document.querySelector("button");
-
-      
