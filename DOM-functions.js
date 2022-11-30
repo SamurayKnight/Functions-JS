499942c767adc30086666b2b053bb86857d8ce39
SamurayKnight <danlee97.06@gmail.com>
2022-11-29 20:07:05 -0600

    Ejercicio 1 parte 1

---
+++ DOM-functions.js
@@ -0,0 +1,6 @@
+//Crear un menú tipo "hamburguesa"
+const $nav = document.querySelector(".navegador"),
+      $section = document.querySelector(".section"),
+      $button = document.querySelector("button");
+
+      
