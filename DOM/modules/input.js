aa42258f01d87c269750ca68a3bc273c948bec85
SamurayKnight <danlee97.06@gmail.com>
2022-12-08 15:16:30 -0600

    Inputs

---
+++ DOM/modules/input.js
@@ -0,0 +1,19 @@
+const d = document;
+
+export default function inputs(form) {
+    const f = d.getElementById(form);
+    let tester;
+
+    d.addEventListener("submit", (e) => {
+        if (e.target === f) {
+            e.preventDefault();
+            tester = window.open(
+                f.direccion.value, 
+                "tester",
+                `innerWidth=${f.ancho.value}, innerHeight = ${f.alto.value}`)
+        }});
+
+    d.addEventListener("click", e => {
+        if (e.target === form.cerrar) tester.close();
+    });
+};
